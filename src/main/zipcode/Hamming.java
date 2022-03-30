66b83b91420a0a195aede574d4a25dc1cb54bcdb
Mike <29170001+mike-prentice@users.noreply.github.com>
2022-03-30 17:18:08 -0400

    a few tests pass

--- src/main/zipcode/Hamming.java
+++ src/main/zipcode/Hamming.java
@@ -1,10 +1,24 @@
 package zipcode;
 
 public class Hamming {
-    public Hamming(String s, String s1) {
-    }
+    String str2 = "";
+    String str3 = "";
 
-    public int getHammingDistance() {
-        return -1;
+    public Hamming(String s, String s1) {
+      str2 = s;
+      str3 = s1;
     }
+    public int getHammingDistance() {
+        int i = 0;
+        int hammingDist = 0;
+        if (str2.length() != str3.length()) {
+            throw new IllegalArgumentException();
+        } else {
+            for (int j = 0; j < str2.length(); j++) {
+                if (str2.charAt(i) != str3.charAt(i)) {
+                    hammingDist++;
+                }
+            } return hammingDist;
+        }
+}
 }
